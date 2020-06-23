0393c22bf7c666e74e80096457b19746cc8d34c8
Kritika <kritikasony42@gmail.com>
2020-06-23 13:43:01 +0530

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tdwl@!torpa224gyz+ifu#h)&b&pd)qb8-+ngmoe4)5rsv1-s_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kritikasony.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
